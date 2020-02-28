56b50494845bc242631a50f01276ad64fee0e642
yujingtian <942843614@qq.com>
2020-02-28 18:11:56 +0800

    训练预测

--- linear/script.js
+++ linear/script.js
@@ -1,20 +1,30 @@
 import * as tfvis from "@tensorflow/tfjs-vis"
+import * as tf from "@tensorflow/tfjs"
 
-window.onload = function(){
+window.onload =async function(){
     const xs = [1,2,3,4]
     const ys = [1,3,5,7]
-    tfvis.render.scatterplot(
+    const predictInput = document.getElementById("predictInput")
+    const predictBtn = document.getElementById("predictBtn")
+
+    const model = tf.sequential();
+    model.add(tf.layers.dense({units:1, inputShape:[1]}))
+    model.compile({ loss:tf.losses.meanSquaredError, optimizer:tf.train.sgd(0.1) })
+    const inputs = tf.tensor(xs)
+    const labels = tf.tensor(ys)
+    await model.fit(inputs, labels,
         {
-            name:"线性回归训练集"
-        },
-        {
-            values:xs.map((x, i)=>{
-                return {x : x, y : ys[i]}
-            })
-        },
-        {
-            xAxisDomain:[0,5],
-            yAxisDomain:[0,8]
-        },
+            batchSize:1,
+            epochs:100,
+            callbacks:tfvis.show.fitCallbacks(
+                {name : "训练过程"},
+                ["loss"]
+            )
+        }
     )
+    predictBtn.classList.remove("hide")
+    predictBtn.onclick = function(){
+        const output = model.predict(tf.tensor([Number(predictInput.value)]))
+        console.log(output.dataSync()[0])
+    }
 }
