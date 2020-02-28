4c66eedec200b8eb98c6ff51320ebbb9a5acf227
yujingtian <942843614@qq.com>
2020-02-28 11:48:15 +0800

    开始学习了哦

---
+++ linear/script.js
@@ -0,0 +1,20 @@
+import * as tfvis from "@tensorflow/tfjs-vis"
+
+window.onload = function(){
+    const xs = [1,2,3,4]
+    const ys = [1,3,5,7]
+    tfvis.render.scatterplot(
+        {
+            name:"线性回归训练集"
+        },
+        {
+            values:xs.map((x, i)=>{
+                return {x : x, y : ys[i]}
+            })
+        },
+        {
+            xAxisDomain:[0,5],
+            yAxisDomain:[0,8]
+        },
+    )
+}
